67d8bf97d5d8385f2d6f30b979f65e54110d3c1b
Alexander-Chudnikov <82392953+Alexander-Chudnikov@users.noreply.github.com>
2021-05-29 02:47:43 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -17,25 +17,25 @@
 #define UNIX_EPOCH              11644473600LL
 #define WINDOWS_MAX_TIME        9223149887999990000LL
 
-Time::
+void Time::
     setUnix64Time(uint64_t u64_time)
     {
         this->unixtime = u64_time;
         return;
     }
-Time::
+void Time::
     setUnixTime(uint64_t u_time)
     {
-        this->setUnix64Time(self, u_time + UNIX_TIME_OFFSET);
+        this->setUnix64Time(u_time + UNIX_TIME_OFFSET);
         return;
     }
-Time::
+void Time::
     setFileTime(uint64_t filetime)
     {
-        this->setUnixTime(filetime / WINDOWS_TICK - SEC_TO_UNIX_EPOCH);
+        this->setUnixTime(filetime / WINDOWS_TICK - UNIX_EPOCH);
         return;
     }
-Time::
+void Time::
     setFull(unsigned int day = 0, unsigned int month = 0, uint64_t year = 0, unsigned int hour = 0, unsigned int minute = 0, unsigned int second = 0)
     {
         if ((year >= 0 && year < (UNIX_TIME_64_MAX_YEAR + 1)) && (month >= 0 && month <= 12) && (day >= 0 && day <= 31) && (hour >= 0 && hour <= 24) && (minute >= 0 && minute <= 60) && (second >= 0 && second <= 60))
@@ -113,84 +113,84 @@ Time::
         }
         std::cerr << "Date is out of range, date must be smaller than 584554049253-10-8T07:00:15+00:00" << std::endl;
     }
-Time::
+void Time::
     setDate(unsigned int day, unsigned int month, uint64_t year)
     {
         this->setFull(day, month, year);
         return;
     }
-Time::
+void Time::
     setTime(unsigned int hour, unsigned int minute, unsigned int second)
     {
         this->setFull(0, 0, 0, hour, minute, second);
         return;
     }
-Time::
+void Time::
     setYear(uint64_t year)
     {
         this->setFull(0, 0, year);
         return;
     }
-Time::
+void Time::
     setMonth(unsigned int month)
     {
         this->setFull(0, month);
         return;
     }
-Time::
+void Time::
     setDay(unsigned int day)
     {
         this->setFull(day);
         return;
     }
-Time::
+void Time::
     setHour(unsigned int hour)
     {
         this->setFull(0, 0, 0, hour);
         return;
     }
-Time::
+void Time::
     setMinute(unsigned int minute)
     {
         this->setFull(0, 0, 0, 0, minute);
         return;
     }
-Time::
+void Time::
     setSecond(unsigned int second)
     {
         this->setFull(0, 0, 0, 0, 0, second);
         return;
     }
-Time::
+uint64_t Time::
     getUnix64Time() const
     {
         return this->unixtime;
     }
-Time::
+uint64_t Time::
     getUnixTime() const
     {
         if ((this->unixtime - UNIX_TIME_OFFSET) > UNIX_TIME_MAX)
         {
-            return UNIX_TIME_MAX
+            return UNIX_TIME_MAX;
         }
         else 
         {
             return (this->unixtime - UNIX_TIME_OFFSET);
         }
     }
-Time::
+uint64_t Time::
     getFileTime() const
     {
-        if ((this->unixtime * WINDOWS_TICK + SEC_TO_UNIX_EPOCH) > WINDOWS_MAX_TIME)
+        if ((this->unixtime * WINDOWS_TICK + UNIX_EPOCH) > WINDOWS_MAX_TIME)
         {
-            return WINDOWS_MAX_TIME
+            return WINDOWS_MAX_TIME;
         }
         else 
         {
-            return (this->unixtime * WINDOWS_TICK + SEC_TO_UNIX_EPOCH);
+            return (this->unixtime * WINDOWS_TICK + UNIX_EPOCH);
         }
     }
-Time:: 
+uint64_t * Time:: 
     getFull() const
     {
         uint64_t time_arr[6];
@@ -202,7 +202,6 @@ Time::
         int day = 0;
         int hour = 0;
         int minutes = 0;
-        int seconds = 0;
 
         if (seconds % 4 == 0)
         {
@@ -271,62 +270,62 @@ Time::
         }
         while (seconds >= 60)
         {
-            ++minute;
+            ++minutes;
             seconds -= 60;
         }
 
         time_arr[1] = month;
         time_arr[2] = day;
         time_arr[3] = hour;
-        time_arr[4] = minute;
+        time_arr[4] = minutes;
         time_arr[5] = seconds;
 
         return time_arr;
     }
-Time::
+uint64_t Time::
     getYear() const 
     {
         uint64_t *time = this->getFull();
         return time[0];
     }
-Time::
+int Time::
     getMonth() const 
     {
         uint64_t *time = this->getFull();
         return time[1];
     }
-Time::
+int Time::
     getDay() const 
     {
         uint64_t *time = this->getFull();
         return time[2];
     }
-Time::
+int Time::
     getHour() const 
     {
         uint64_t *time = this->getFull();
         return time[3];
     }
-Time::
+int Time::
     getMinute() const 
     {
         uint64_t *time = this->getFull();
         return time[4];
     }
-Time::
+int Time::
     getSecond() const 
     {
         uint64_t *time = this->getFull();
         return time[5];
     }
 
-std::ostream& Time::operator<< (std::ostream &out, const Time& right)
+std::ostream& operator<< (std::ostream &out, const Time& right)
 {
-    uint64_t *time = right->getFull();
+    uint64_t *time = right.getFull();
 	out << std::setw(4) << std::setfill('0') << time[0] << '-' << std::setw(2) << std::setfill('0') << time[1] << '-' << time[2] << "T" << time[3] << ":" << time[4] << ":" << time[5] << "+00:00" << std::endl;
 	return out;
 }
-std::istream& Time::operator>> (std::istream &in, Time& right)
+std::istream& operator>> (std::istream &in, Time& right)
 {
     unsigned int day = 0;
     unsigned int month = 0; 
@@ -342,13 +341,13 @@ std::istream& Time::operator>> (std::istream &in, Time& right)
     in >> minute;
     in >> second;
 
-    return int;
+    return in;
 }
-Time Time::operator +(Time const& left, Time const& right) 
+Time operator +(const Time& left, const Time& right) 
 {
-    try ()
+    try 
     {
-        return(left->getUnix64Time() + right->getUnix64Time());
+        return(left.getUnix64Time() + right.getUnix64Time());
     }
     catch (...)
     {
@@ -356,11 +355,11 @@ Time Time::operator +(Time const& left, Time const& right)
         return 0;
     }
 }
-Time Time::operator -(Time const& left, Time const& right) 
+Time operator -(const Time& left, const Time& right) 
 {
-    try ()
+    try 
     {
-        return (left->getUnix64Time() - right->getUnix64Time());
+        return (left.getUnix64Time() - right.getUnix64Time());
     }
     catch (...)
     {
@@ -368,55 +367,27 @@ Time Time::operator -(Time const& left, Time const& right)
         return 0;
     }
 }
-Time& Time::operator +=(Time const& left, Time const& right) 
+bool operator ==(const Time& left, const Time& right)
 {
-    uint64_t l_u_time = left->getUnix64Time();
-    uint64_t r_u_time = right->getUnix64Time();
-    try ()
-    {
-        left->setUnix64Time(l_u_time += r_u_time);
-    }
-    catch (...)
-    {
-        std::cerr << "UnixDate is out of range, UnixDate must be smaller than 18446744073709551615" << std::endl;
-    }
-    return left;
+    return (left.getUnix64Time() == right.getUnix64Time());
 }
-Time& Time::operator -=(Time const& left, Time const& right) 
+bool operator !=(const Time& left, const Time& right)
 {
-    uint64_t l_u_time = left->getUnix64Time();
-    uint64_t r_u_time = right->getUnix64Time();
-    try ()
-    {
-        left->setUnix64Time(l_u_time -= r_u_time);
-    }
-    catch (...)
-    {
-        std::cerr << "UnixDate is out of range, UnixDate must be bigger than 0" << std::endl;
-    }
-    return left;
+    return (left.getUnix64Time() != right.getUnix64Time());
 }
-bool Time::operator ==(const Time& left, const Time& right)
+bool operator <=(const Time& left, const Time& right)
 {
-    return (left->getUnix64Time() == right->getUnix64Time());
+    return (left.getUnix64Time() <= right.getUnix64Time());
 }
-bool Time::operator !=(const Time& left, const Time& right)
+bool operator >=(const Time& left, const Time& right)
 {
-    return (left->getUnix64Time() != right->getUnix64Time());
+    return (left.getUnix64Time() >= right.getUnix64Time());
 }
-bool Time::operator <=(const Time& left, const Time& right)
+bool operator >(const Time& left, const Time& right)
 {
-    return (left->getUnix64Time() <= right->getUnix64Time());
+    return (left.getUnix64Time() > right.getUnix64Time());
 }
-bool Time::operator >=(const Time& left, const Time& right)
+bool operator <(const Time& left, const Time& right)
 {
-    return (left->getUnix64Time() >= right->getUnix64Time());
-}
-bool Time::operator >(const Time& left, const Time& right)
-{
-    return (left->getUnix64Time() > right->getUnix64Time());
-}
-bool Time::operator <(const Time& left, const Time& right)
-{
-    return (left->getUnix64Time() < right->getUnix64Time());
+    return (left.getUnix64Time() < right.getUnix64Time());
 }
