52629aa94befeba402f36d69ad7ff306834d1297
Alexander-Chudnikov <82392953+Alexander-Chudnikov@users.noreply.github.com>
2021-06-09 01:57:12 +0300

    Add files via upload

--- lab.cpp
+++ lab.cpp
@@ -1,6 +1,7 @@
 #include "lab.h"
 
 #include <iostream>
+#include <string>
 #include <iomanip>
 
 #define UNIX_TIME_OFFSET        62167132800LL
@@ -16,12 +17,278 @@
 #define WINDOWS_TICK            10000000
 #define UNIX_EPOCH              11644473600LL
 #define WINDOWS_MAX_TIME        9223149887999990000LL
+#define REMINDER_M_CONSTANT_15  532
+#define REMINDER_M_CONSTANT_28  285
+#define REMINDER_M_CONSTANT_19  420
+#define REMINDER_M_CONSTANT     7980
+#define NOT_FOUND               18446744073709551615ULL
+/*
+inline int euclidGCD(int num_1, int num_2) 
+{
+    if (num_1 == num_2) 
+    {
+        return num_1;
+    }
+    if (num_1 > num_2) {
+        int temp = num_1;
+        num_1 = num_2;
+        num_2 = temp;
+    }
+    return euclidGCD(num_1, num_2 - num_1);
+} 
+*/
+/*  
+ * 
+ */
 
+/*                   Chinese theorem of reminders with predetermend values 
+ * y_* are storing values that are nesseesary for acquiring certan reminder
+ * rem_* are values, that store reminders, from wich later y_* will be calculted
+ * REMINDER_M_CONSTANT* are predetermined values, that can be calculeted by multiplying two other dividers
+ */ 
 
+inline int remindersToNum(int rem_1, int rem_2, int rem_3)
+{
+    int y_1 = 1;
+    int y_2 = 1;
+    int y_3 = 1;
+    while ((REMINDER_M_CONSTANT_15 * y_1 % 15 != 1) and (y_1 < 15))
+    {
+        ++y_1;
+    }
+    while ((REMINDER_M_CONSTANT_28 * y_2 % 28 != 1) and (y_2 < 28))
+    {
+        ++y_2;
+    }
+    while ((REMINDER_M_CONSTANT_19 * y_3 % 19 != 1) and (y_3 < 19))
+    {
+        ++y_3;
+    }
+    return ((REMINDER_M_CONSTANT_15 * y_1 * rem_1)+(REMINDER_M_CONSTANT_28 * y_2 * rem_2)+(REMINDER_M_CONSTANT_19 * y_3 * rem_3)) % REMINDER_M_CONSTANT;
+}
+
+/*                   Function that check is string contains only numbers 
+ */ 
+
+inline bool isNum(std::string  const& string){
+    return string.find_first_not_of("0123456789") == std::string::npos;
+}
+
+/*                   String to uint64_t convertion 
+ * Simple coversion algorythm, that is based on string to char conversion with further byte wrighting to result;
+ */ 
+
+inline uint64_t stringToUint64_t(std::string const& value) 
+{
+    if (isNum(value))
+    {
+        uint64_t result = 0;
+
+        char const* char_v = value.c_str();
+        char const* char_q = char_v + value.size();
+        while (char_v < char_q) 
+        {
+            result = (result << 1) + (result << 3) + *(char_v++) - '0';
+        }
+        return result;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+/*                   String to unsingned int convertion 
+ * String converted to unsigned long, than checked to be in range of unsigned int
+ */
+
+inline unsigned int stringToUint( std::string const& string)
+{
+    if (isNum(string))
+    {
+        unsigned long lresult = stoul(string, 0, 10);
+        unsigned int result = lresult;
+        if (result != lresult) 
+        {
+            std::cerr << result << " is out of range" << std::endl;
+        }
+        return result;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+/*                  This function checks if inputed indicts are coorrect
+ * After check, it will get year from remindersToNum function and pass it to setBullshitYear
+ */
+
+void Time::
+    setBullshitTime(int indict, int solar_shit, int lunar_shit)
+    {
+        if ((indict > 15) or (indict < 1) or (solar_shit > 28) or (solar_shit < 1) or (lunar_shit > 19) or (lunar_shit < 1))
+        {
+            if ((indict > 15) or (indict < 1))
+            {
+                std::cerr << "Indict is out of range" << std::endl;
+            }
+            if ((solar_shit > 28) or (solar_shit < 1))
+            {
+                std::cerr << "Solar circle is out of range" << std::endl;
+            }
+            if ((lunar_shit > 19) or (lunar_shit < 1))
+            {
+                std::cerr << "Lunar circle is out of range" << std::endl;
+            }
+            this->unixtime = 0;
+        }
+        else 
+        {
+            this->setBullshitYear(remindersToNum(indict, solar_shit, lunar_shit));
+        }
+        return;
+    }
 void Time::
     setUnix64Time(uint64_t u64_time)
     {
         this->unixtime = u64_time;
+        return;
+    }
+
+/*                  This function parse input string
+ * 
+ */
+
+void Time::
+    setStringTime(std::string str_time)
+    {
+        std::string delimiter_1 = "T";
+        std::string delimiter_2 = "-";
+        std::string delimiter_3 = ":";
+        std::string delimiter_4 = "+";
+        std::string delimiter_5 = "-";
+        std::string str_date;
+        std::string str_day;
+        std::string str_month;
+        std::string str_year;
+        std::string str_hour;
+        std::string str_minute;
+        std::string str_second;
+        std::string str_g_hour;
+
+        if(str_time.find(delimiter_1) != NOT_FOUND)
+        {
+            str_date = str_time.substr(0, str_time.find(delimiter_1));
+            str_time.erase(0, str_time.find(delimiter_1) + delimiter_1.length());
+        }
+        else
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+            return;
+        }
+
+        if(str_date.find(delimiter_2) != NOT_FOUND)
+        {
+            str_year = str_date.substr(0, str_date.find(delimiter_2));
+            str_date.erase(0, str_date.find(delimiter_2) + delimiter_2.length());
+        }
+        else
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+            return;
+        }
+
+        if(str_date.find(delimiter_2) != NOT_FOUND)
+        {
+            str_month = str_date.substr(0, str_date.find(delimiter_2));
+            str_date.erase(0, str_date.find(delimiter_2) + delimiter_2.length());
+            str_day = str_date;
+        }
+        else
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+            return;
+        }
+
+        if(str_time.find(delimiter_3) != NOT_FOUND)
+        {
+            str_hour = str_time.substr(0, str_time.find(delimiter_3));
+            str_time.erase(0, str_time.find(delimiter_3) + delimiter_3.length());
+        }
+        else
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+            return;
+        }
+
+        if(str_time.find(delimiter_3) != NOT_FOUND)
+        {
+            str_minute = str_time.substr(0, str_time.find(delimiter_3));
+            str_time.erase(0, str_time.find(delimiter_3) + delimiter_3.length());
+        }
+        else
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+            return;
+        }
+
+        if(str_time.find(delimiter_4) != NOT_FOUND or str_time.find(delimiter_5) != NOT_FOUND)
+        {
+            if (str_time[2] == '+')
+            {
+                str_second = str_time.substr(0, str_time.find(delimiter_4));
+                str_time.erase(0, str_time.find(delimiter_4) + delimiter_4.length());
+            }
+            else
+            {
+                str_second = str_time.substr(0, str_time.find(delimiter_5));
+                str_time.erase(0, str_time.find(delimiter_5) + delimiter_5.length());
+            }
+            if(str_time.find(delimiter_3) != NOT_FOUND)
+            {
+                str_g_hour = str_time.substr(0, str_time.find(delimiter_3));
+                str_time.erase(0, str_time.find(delimiter_3) + delimiter_3.length());
+            }
+            else
+            {
+                std::cerr << "Invalid string format given" << std::endl;
+                this->unixtime = 0;
+                return;
+            }
+        }
+        else
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+            return;
+        }
+
+        /*           TODO
+         * • Add GTM time shift 
+         * • Fix number length 0 error 
+         * • Remove debug output
+         * • Clean this part 
+         * • Make single error handler
+         * • Complete comment
+         */
+
+        std::cout << str_g_hour << " " << str_time << std::endl;
+
+        if (isNum(str_day) && isNum(str_month) && isNum(str_year) && isNum(str_hour) && isNum(str_minute) && isNum(str_second))
+        {
+           this->setFull(stringToUint(str_day), stringToUint(str_month), stringToUint64_t(str_year), stringToUint(str_hour), stringToUint(str_minute), stringToUint(str_second)); 
+        }
+        else 
+        {
+            std::cerr << "Invalid string format given" << std::endl;
+            this->unixtime = 0;
+        }
         return;
     }
 void Time::
@@ -36,8 +303,14 @@ void Time::
         this->setUnixTime(filetime / WINDOWS_TICK - UNIX_EPOCH);
         return;
     }
+
+/*                  This function convert date and time into seconds
+ * After simple user input validation, function determinies if year is a leap year 
+ * Then goes calculation of days in month with correction for leap years and amount of leap years since start of an epoch
+ */
+
 void Time::
-    setFull(unsigned int day = 1, unsigned int month = 1, uint64_t year = 0, unsigned int hour = 0, unsigned int minute = 0, unsigned int second = 0)
+    setFull(unsigned int day , unsigned int month , uint64_t year = 0, unsigned int hour = 0, unsigned int minute = 0, unsigned int second = 0)
     {
         if ((year >= 0 && year < (UNIX_TIME_64_MAX_YEAR + 1)) && (month > 0 && month <= 12) && (day > 0 && day <= 31) && (hour >= 0 && hour <= 24) && (minute >= 0 && minute <= 60) && (second >= 0 && second <= 60))
         {
@@ -184,55 +457,109 @@ void Time::
 void Time::
     setDate(unsigned int day, unsigned int month, uint64_t year)
     {
-        this->setFull(day, month, year);
+        this->setFull(day, month, year, this->getHour(), this->getMinute(), this->getSecond());
         return;
     }
 void Time::
     setTime(unsigned int hour, unsigned int minute, unsigned int second)
     {
-        this->setFull(0, 0, 0, hour, minute, second);
+        this->setFull(this->getDay(), this->getMonth(), this->getYear(), hour, minute, second);
         return;
+    }
+void Time::
+    setBullshitYear(uint64_t stupid_year)
+    {
+        if (stupid_year - 5508 <= 2470){
+            std::cerr << (stupid_year - 5508) << std::endl;
+            this->setFull(this->getDay(), this->getMonth(), (stupid_year - 5508), this->getHour(), this->getMinute(), this->getSecond());
+        }
+        else
+        {
+            std::cerr << "Year is out of range" << std::endl;
+            this->unixtime = 0;
+        }
     }
 void Time::
     setYear(uint64_t year)
     {
-        this->setFull(0, 0, year);
+        this->setFull(this->getDay(), this->getMonth(), year, this->getHour(), this->getMinute(), this->getSecond());
         return;
     }
 void Time::
     setMonth(unsigned int month)
     {
-        this->setFull(0, month);
+        this->setFull(this->getDay(), month, this->getYear(), this->getHour(), this->getMinute(), this->getSecond());
         return;
     }
 void Time::
     setDay(unsigned int day)
     {
-        this->setFull(day);
+        this->setFull(day, this->getMonth(), this->getYear(), this->getHour(), this->getMinute(), this->getSecond());
         return;
     }
 void Time::
     setHour(unsigned int hour)
     {
-        this->setFull(0, 0, 0, hour);
+        this->setFull(this->getDay(), this->getMonth(), this->getYear(), hour, this->getMinute(), this->getSecond());
         return;
     }
 void Time::
     setMinute(unsigned int minute)
     {
-        this->setFull(0, 0, 0, 0, minute);
+        this->setFull(this->getDay(), this->getMonth(), this->getYear(), this->getHour(), minute, this->getSecond());
         return;
     }
 void Time::
     setSecond(unsigned int second)
     {
-        this->setFull(0, 0, 0, 0, 0, second);
+        this->setFull(this->getDay(), this->getMonth(), this->getYear(), this->getHour(), this->getMinute(), second);
         return;
     }
+
 uint64_t Time::
     getUnix64Time() const
     {
         return this->unixtime;
+    }
+std::string Time::
+    getStringTime() const
+    {
+        uint64_t *time = this->getFull();
+        std::string out = std::to_string(time[0]);
+        out.append("-");
+        out.append(std::to_string(time[1]));
+        out.append("-");
+        out.append(std::to_string(time[2]));
+        out.append("T");
+        out.append(std::to_string(time[3]));
+        out.append(":");
+        out.append(std::to_string(time[4]));
+        out.append(":");
+        out.append(std::to_string(time[5]));
+        out.append("+00:00");
+        return out;
+    }
+int * Time::
+    getBullshitIndict() const
+    {
+        static int indicts[3];
+        indicts[0] = this->getBullshitYear() % 15;
+        indicts[1] = this->getBullshitYear() % 28;
+        indicts[2] = this->getBullshitYear() % 19;
+        return indicts;
+    }
+uint64_t Time::
+    getBullshitYear() const
+    {
+        if (584554049253LL - this->getYear() > 5508)
+        {
+            return (this->getYear() + 5508);
+        }
+        else 
+        {
+            std::cerr << "Year is out of range" << std::endl;
+            return 0;
+        }
     }
 uint64_t Time::
     getUnixTime() const
@@ -258,6 +585,13 @@ uint64_t Time::
             return (this->unixtime * WINDOWS_TICK + UNIX_EPOCH);
         }
     }
+
+/*                  This function returns date and time in array
+ * Function determines if current year is leap, than goes prediction of unixtime and leap years after what
+ * Predicted unixtime being compared with original one, then years being adjusted so they are not bigger than 
+ * Original year, at the end goes month, days, hours and minutes calculation
+ */
+
 uint64_t * Time:: 
     getFull() const
     {
@@ -435,8 +769,8 @@ int Time::
 std::ostream& operator<< (std::ostream &out, const Time& right)
 {
     uint64_t *time = right.getFull();
-	out << std::setw(4) << std::setfill('0') << time[0] << '-' << std::setw(2) << std::setfill('0') << time[1] << '-' << time[2] << "T" << time[3] << ":" << time[4] << ":" << time[5] << "+00:00" << std::endl;
-	return out;
+    out << std::setw(4) << std::setfill('0') << time[0] << '-' << std::setw(2) << std::setfill('0') << time[1] << '-' << time[2] << "T" << time[3] << ":" << time[4] << ":" << time[5] << "+00:00";
+    return out;
 }
 std::istream& operator>> (std::istream &in, Time& right)
 {
@@ -520,7 +854,7 @@ Time::
         this->unixtime = unixtime64;
     }
 Time::
-    Time(unsigned int day, unsigned int month, uint64_t year, unsigned int hour, unsigned int minute, unsigned int second)
+    Time(unsigned int day , unsigned int month , uint64_t year = 0, unsigned int hour = 0, unsigned int minute = 0, unsigned int second = 0)
     {
         this->setFull(day, month, year, hour, minute, second);
     }
@@ -541,7 +875,7 @@ int main()
     std::cout << "Input time: 2002-10-23 22:13:55" << std::endl;
 
     Time time(23, 10, 2002, 22, 13, 55);
-    std::cout << time;
+    time.setStringTime("2033-10-23T22:13:55+00:00");
+    std::cout << time << std::endl;
+    std::cout << time.getBullshitIndict()[0] << " "<< time.getBullshitIndict()[1] << " "<< time.getBullshitIndict()[2];
 }
-
-
--- lab.h
+++ lab.h
@@ -3,15 +3,39 @@
 
 #include <iostream> 
 #include <stdint.h> 
+#include <string>
 
 class Time
 {
 private:
+
+    // Date stored in uint64_t format (second since 0-1-1 0:0:0 up to 584554049253-10-8 7:0:15)
+
     uint64_t unixtime;
-    uint64_t leap_years;
 public:
-    
+
+    /*                      Set Functions
+     * Function that set date and time in uint64_t format with uint64_t number
+     * Function that set date and time in uint64_t format with string value 
+     * Function that set year in uint64_t format using indict and 2 other usless and squalid variables
+     * Function that set year in uint64_t format with stupid year format 
+     * Function that set date and time in uint64_t format using date and time in unixtime format 
+     * Function that set date and time in uint64_t format using date and time in filetime format
+     * Function that set date and time in uint64_t format with date and time in format: (DAY-MONTH-YEAR HOUR:MINUTE:SECOND)
+     * Function that set date in uint64_t format with date in format: (DAY-MONTH-YEAR)
+     * Function that set time in uint64_t format with time in format: (HOUR:MINUTE:SECOND)
+     * Function that set year in uint64_t format
+     * Function that set month in unsigned int format
+     * Function that set day in unsigned int format
+     * Function that set hour in unsigned int format
+     * Function that set minute in unsigned int format
+     * Function that set second in unsigned int format
+     */
+
     void setUnix64Time(uint64_t u64_time);
+    void setStringTime(std::string str_time);
+    void setBullshitTime(int indict, int solar_shit, int lunar_shit);
+    void setBullshitYear(uint64_t stupid_year);
     void setUnixTime(uint32_t u_time);
     void setFileTime(uint64_t filetime);
     void setFull(unsigned int day, unsigned int month, uint64_t year, unsigned int hour, unsigned int minute, unsigned int second);
@@ -24,17 +48,43 @@ public:
     void setMinute(unsigned int minute);
     void setSecond(unsigned int second);
     
-    uint64_t getUnix64Time() const;
-    uint64_t getUnixTime()   const;
-    uint64_t getFileTime()   const;
-    uint64_t * getFull()     const;
-    uint64_t getYear()       const;
-    int      getMonth()      const;
-    int      getDay()        const;
-    int      getHour()       const;
-    int      getMinute()     const;
-    int      getSecond()     const;
+    /*                      Get Functions              
+     * Function that return date in uint64_t format
+     * Function that return date in string format
+     * Function that return link to array with indicts of stupid time 
+     * Function that return year in shabby year
+     * Function that return unixtime
+     * Function that return filetime 
+     * Function that return link to array with date and time 
+     * Function that return year
+     * Function that return month 
+     * Function that return hour
+     * Function that return minute
+     * Function that return second
+     */
+
+    uint64_t    getUnix64Time()     const;
+    std::string getStringTime()     const;
+    int *       getBullshitIndict() const;
+    uint64_t    getBullshitYear()   const;
+    uint64_t    getUnixTime()       const;
+    uint64_t    getFileTime()       const;
+    uint64_t *  getFull()           const;
+    uint64_t    getYear()           const;
+    int         getMonth()          const;
+    int         getDay()            const;
+    int         getHour()           const;
+    int         getMinute()         const;
+    int         getSecond()         const;
     
+    /*                      Constructors
+     * Default constructor 
+     * Constructor that set date and time in unixtime format 
+     * Constructor that set date and time in uint64_t format 
+     * Constructor that set date and time in uint64_t format with date and time in format: (DAY-MONTH-YEAR HOUR:MINUTE:SECOND)
+     * Destructor 
+     * Copy constructor
+     */
 	Time();
     Time(uint32_t unixtime32);
     Time(uint64_t unixtime64);
@@ -42,9 +92,17 @@ public:
    ~Time();
     Time(const Time &time1);
 
+    /*                      Input/Output operator overload 
+     * Formated output overload 
+     * Formated input overload
+     */ 
     friend std::ostream& operator<< (std::ostream &out, const Time& right);
     friend std::istream& operator>> (std::istream &in, Time& right);
 };
+
+/*                      Operator overload 
+ * Other operator overload
+ */ 
 
 Time operator +(const Time& left, const Time& right);
 Time operator -(const Time& left, const Time& right);
